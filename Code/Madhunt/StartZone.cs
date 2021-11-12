fb4993459617cdffeeacbdd21ec259bbab2bd389
Popax21 <popa21@popa21.at>
2021-11-12 18:29:21 +0100

    Final v1.1 preperations

--- Code/Madhunt/StartZone.cs
+++ Code/Madhunt/StartZone.cs
@@ -1,8 +1,10 @@
 using Microsoft.Xna.Framework;
 using Monocle;
+using Celeste.Mod.Entities;
 
 namespace Celeste.Mod.Madhunt {
     [Tracked]
+    [CustomEntity("Madhunt/StartZone")]
     public class StartZone : Trigger {
         public StartZone(EntityData data, Vector2 offset) : base(data, offset) => ID = data.ID;
         public int ID { get; }
