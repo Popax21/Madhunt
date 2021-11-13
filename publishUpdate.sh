3f63f4d03413c5b3f8de442d41ecee39b6f21cb6
Popax21 <29798799+Popax21@users.noreply.github.com>
2021-11-13 17:41:59 +0100

    Added automatic update script

---
+++ publishUpdate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+#Check usage
+if [ $# -ne 1 ] 
+then
+    echo "Usage: $0 <version>"
+    exit
+fi
+
+#Create the zip file
+./createZip.sh
+
+#Let user enter changelog
+changelog="$(mktemp).md"
+echo "Enter changelog for version $1 here" > $changelog
+xdg-open $changelog
+echo "Enter changelog (your default editor should have opened)"
+read -p "Press enter when done..."
+
+#Get confirmation before publishing
+echo
+echo "Are you sure you want to publish version $1?"
+echo "Changelog:"
+cat $changelog
+echo
+
+select conf in "Yes" "No"; do
+    case $conf in
+        Yes ) break;;
+        * ) echo "Aborting..."; exit;;
+    esac
+done
+
+#Add changelog to CHANGELOG.txt
+changelog2="$(mktemp)"
+echo "---------- CHANGELOG VERSION $1 ----------" >> $changelog2
+cat $changelog >> $changelog2
+echo >> $changelog2
+echo >> $changelog2
+cat CHANGELOG.txt >> $changelog2
+mv -f $changelog2 CHANGELOG.txt
+
+#Create release on GitHub
+commit="$(git log -n 1 --pretty=format:%H main)"
+git commit -m "Automated update to version $1" CHANGELOG.txt
+git tag -a -m "Version $1" v$1 $commit
+git push origin main v$1
+gh release create v$1 -F $changelog --target $commit
